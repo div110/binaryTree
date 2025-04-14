6940111c21200673796b3fefd773454d95779ac2
div110 <davidxhalamka@gmail.com>
2025-04-14 15:36:23 +0200

    :/

--- bst/bst.c
+++ bst/bst.c
@@ -1,91 +1,97 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct node {
   int32_t value;
-  struct node * leftChild;
-  struct node * rightChild;
+  struct node *leftChild;
+  struct node *rightChild;
 };
 
-void insert(int32_t newValue, struct node * rootNode){
-  
+void insert(int32_t newValue, struct node *rootNode) {
+
   bool end = false;
-  struct node * currentNode=rootNode;
-  
-  while (end==false){
-  
-    if(newValue<=currentNode->value){
+  struct node *currentNode = rootNode;
 
-      if(currentNode->leftChild==NULL){
-        struct node * newNode=(struct node *)malloc(sizeof(struct node));
-        newNode->value=newValue;
-        currentNode->leftChild=newNode;
-        end=true;
+  while (end == false) {
+
+    if (newValue <= currentNode->value) {
+
+      if (currentNode->leftChild == NULL) {
+        struct node *newNode = (struct node *)malloc(sizeof(struct node));
+        newNode->value = newValue;
+        currentNode->leftChild = newNode;
+        end = true;
+      } else if (currentNode->leftChild != NULL) {
+        currentNode = currentNode->leftChild;
       }
-      else if(currentNode->leftChild!=NULL){currentNode=currentNode->leftChild;}
     }
-    
+
     else {
-    if(newValue>currentNode->value){
-        if(currentNode->rightChild!=NULL){
-          currentNode=currentNode->rightChild;
-        }
-        else if(currentNode->rightChild==NULL){
-          struct node * newNode=(struct node *)malloc(sizeof(struct node));
-          newNode->value=newValue;
-          currentNode->rightChild=newNode;
-          end=true;
+      if (newValue > currentNode->value) {
+        if (currentNode->rightChild != NULL) {
+          currentNode = currentNode->rightChild;
+        } else if (currentNode->rightChild == NULL) {
+          struct node *newNode = (struct node *)malloc(sizeof(struct node));
+          newNode->value = newValue;
+          currentNode->rightChild = newNode;
+          end = true;
         }
       }
-
     }
   }
-
 }
-void delete(int32_t deleteValue,struct * node rootNode){
-  struct * node previousNode = NULL;
-  struct * node currentNode = rootNode; 
+void delete(int32_t deleteValue, struct node *rootNode) {
+  struct node *previousNode = NULL;
+  struct node *currentNode = rootNode;
   bool origin = false;
-  (previousNode->leftChild==currentNode) ? origin = true : origin = false;
-  
-  if(currentNode->value == deleteValue){
-    
-    if(currentNode->leftChild!=NULL && currentNode->rightChild!=NULL){
-       
-    }
-    else if (currentNode->leftChild != NULL && currentNode->rightChild == NULL){
-      (origin) ? previousNode->leftChild = currentNode->leftChild : previousNode->rightChild = currentNode->leftChild;
-      free(currentNode);
-    }
-    else if (currentNode->rightChild != NULL && currentNode->leftChild == NULL){
-      (origin) ? previousNode->leftChild = currentNode->rightChild : previousNode->rightChild = currentNode->rightChild;
-      free(currentNode);
-    }
-    else if (currentNode->leftChild == NULL && currentNode->rightChild == NULL){
-      if(previousNode->leftChild == currentNode){previousNode->leftChild = NULL;}
-      else if(previousNode->rightChild == currentNode){previousNode->rightChild = NULL;}
-      free(currentNode);
-    }
+  origin = (previousNode->leftChild == currentNode);
 
+  if (currentNode->value == deleteValue) {
 
+    if (currentNode->leftChild != NULL && currentNode->rightChild != NULL) {
+
+    } else if (currentNode->leftChild != NULL &&
+               currentNode->rightChild == NULL) {
+      if (origin) {
+        previousNode->leftChild = currentNode->leftChild;
+      } else {
+        previousNode->rightChild = currentNode->leftChild;
+      }
+      free(currentNode);
+    } else if (currentNode->rightChild != NULL &&
+               currentNode->leftChild == NULL) {
+      if (origin) {
+        previousNode->leftChild = currentNode->rightChild;
+      } else {
+        previousNode->rightChild = currentNode->rightChild;
+      }
+      free(currentNode);
+    } else if (currentNode->leftChild == NULL &&
+               currentNode->rightChild == NULL) {
+      if (previousNode->leftChild == currentNode) {
+        previousNode->leftChild = NULL;
+      } else if (previousNode->rightChild == currentNode) {
+        previousNode->rightChild = NULL;
+      }
+      free(currentNode);
+    }
   }
 }
 
-int main(){
-struct node * rootNode=(struct node *)malloc(sizeof(struct node));
-//struct node rootNode;
+int main() {
+  struct node *rootNode = (struct node *)malloc(sizeof(struct node));
+  // struct node rootNode;
   rootNode->value = 8;
-  rootNode->leftChild=NULL;
-  rootNode->rightChild=NULL;
-  
-  insert(0,rootNode);
-  insert(5,rootNode);
-  printf("%d\n",rootNode->value);
-  printf("%d\n",rootNode->leftChild->value);
-  printf("%d\n",rootNode->leftChild->rightChild->value);
+  rootNode->leftChild = NULL;
+  rootNode->rightChild = NULL;
 
+  insert(0, rootNode);
+  insert(5, rootNode);
+  printf("%d\n", rootNode->value);
+  printf("%d\n", rootNode->leftChild->value);
+  printf("%d\n", rootNode->leftChild->rightChild->value);
 
   return 0;
 }
