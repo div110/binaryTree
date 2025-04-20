781d6042e52d695da74d9eea7caa80e062ee2ce7
div110 <davidxhalamka@gmail.com>
2025-04-20 16:43:04 +0200

    2class

---
+++ Python/bst/bst.py
@@ -0,0 +1,10 @@
+class Tree:
+    def __init__(self):
+        self.root = None
+
+
+class Node(self,value, leftchild, rightchild):
+    def __init__:
+        self.value = value
+        self.leftchild = leftchild
+        self.rightchild = rightchild
