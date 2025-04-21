61f17cbbb01a8c9b8e6224b0963508ec9dcc15c3
div110 <davidxhalamka@gmail.com>
2025-04-21 21:46:21 +0200

    PY:BST:nremove()

--- Python/bst/bst.py
+++ Python/bst/bst.py
@@ -6,8 +6,7 @@ class Tree:
         node = Node(value,None,None)
         root = self.root
         #print(self.root)
-        if root == None:
-            
+        if root == None: 
             self.root = node 
             return
         while True:
@@ -51,6 +50,36 @@ class Tree:
                 else:
                     root = root.rightchild
 
+    def remove(self, value_to_delete):
+    
+        root = self.root
+        if root == None:
+            print("tree is empty")
+            return
+        while True:
+            if value_to_delete < root.value and root.leftchild != None:
+                temp = node
+                root = temp
+                node = root.leftchild
+                print("went left")
+            elif value_to_delete > root.value and root.rightchild != None:
+                temp = node
+                root = temp
+                node = root.rightchild
+                print("went right")
+            
+            if node.value == value_to_delete:
+                if node.leftchild == None and node.rightchild == None:
+                    if node == root.leftchild:
+                        root.leftchild = None
+                    elif node == root.rightchild:
+                        root.rightchild = None
+                    print(value_to_delete,"deleted")
+                    return
+
+
+
+
 
 class Node:
     def __init__(self,value, leftchild, rightchild):
@@ -63,10 +92,10 @@ print(strom.root)
 strom.add(1)
 strom.add(2)
 strom.add(3)
-strom.add(4)
-strom.add(5)
-strom.add(6)
-strom.add(7)
 
+strom.find(1)
+strom.find(2)
 strom.find(3)
 
+strom.remove(3)
+strom.find(3)
