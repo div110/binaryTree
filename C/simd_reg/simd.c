18d91da6e5f94cbbf9102bdf3bf5e1c1d37bc60d
div110 <davidxhalamka@gmail.com>
2025-05-20 13:25:54 +0200

    C:SIMD

---
+++ C/simd_reg/simd.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <xmmintrin.h>
+
+int main() {
+
+  __m128 vector;
+  __m128 vector2;
+  __m128 vector3;
+  __m128 vector4;
+  __m128 vector5;
+  __m128 vector6;
+  __m128 vector7;
+  __m128 vector8;
+  __m128 vector9;
+  __m128 vector10;
+  __m128 vector11;
+  __m128 vector12;
+  __m128 vector13;
+  __m128 vector14;
+  __m128 vector15;
+  __m128 vector16;
+  __m128 vector17;
+  __m128 vector18;
+  __m128 vector19;
+  __m128 vector20;
+  __m128 vector21;
+  __m128 vector22;
+  __m128 vector23;
+  __m128 vector24;
+
+  printf("%p\n", &vector);
+  printf("%p\n", &vector2);
+  printf("%p\n", &vector3);
+  printf("%p\n", &vector4);
+  printf("%p\n", &vector5);
+  printf("%p\n", &vector6);
+  printf("%p\n", &vector7);
+  printf("%p\n", &vector8);
+  printf("%p\n", &vector9);
+  printf("%p\n", &vector10);
+  printf("%p\n", &vector11);
+  printf("%p\n", &vector12);
+  printf("%p\n", &vector13);
+  printf("%p\n", &vector14);
+  printf("%p\n", &vector15);
+  printf("%p\n", &vector16);
+  printf("%p\n", &vector17);
+  printf("%p\n", &vector18);
+  printf("%p\n", &vector19);
+  printf("%p\n", &vector20);
+  printf("%p\n", &vector21);
+  printf("%p\n", &vector22);
+  printf("%p\n", &vector23);
+  printf("%p\n", &vector24);
+
+  return 0;
+}
