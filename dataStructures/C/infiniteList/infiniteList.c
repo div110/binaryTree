8a46641fc3f9c6346fadf8459da107efd16370d6
div110 <davidxhalamka@gmail.com>
2025-05-29 23:20:37 +0200

    dataStructures:C:infiniteList

--- dataStructures/C/infiniteList/infiniteList.c
+++ dataStructures/C/infiniteList/infiniteList.c
@@ -5,19 +5,56 @@
 
 struct array {
   uint16_t size;
-  uint16_t free;
+  uint16_t last;
   int *data;
 };
 
-int append(struct array *array, int64_t value) { return 0; }
+int append(struct array *array, int64_t value) {
+
+  if (array->size == array->last) {
+    array->size = array->size * 2;
+    array->data = realloc(array->data, array->size * sizeof(int));
+  }
+  array->data[array->last] = value;
+  array->last++;
+
+  return 0;
+}
+
+void printArray(struct array *array) {
+  for (int i = 0; i < array->last; i++) {
+    printf("%d|", array->data[i]);
+  }
+  for (int i = 0; i < array->size - array->last; i++) {
+    printf("_|");
+  }
+  printf("\n");
+}
+
+void initArray(struct array *myArray) {
+  myArray->data = malloc(2 * sizeof(int));
+  myArray->size = 2;
+  myArray->last = 0;
+  printf("array initialized\n");
+}
 
 int main() {
 
-  struct array array1;
-  array1.size = 5;
-  array1.free = 5;
-  array1.data = NULL;
-  append(&array1, 5);
+  struct array myArray;
+  initArray(&myArray);
+  printArray(&myArray);
+  append(&myArray, 1);
+  append(&myArray, 2);
+  append(&myArray, 4);
+  append(&myArray, 8);
+  append(&myArray, 16);
+  append(&myArray, 1);
+  append(&myArray, 2);
+  append(&myArray, 4);
+  append(&myArray, 8);
+  append(&myArray, 16);
+  printArray(&myArray);
+  printf("size: %d\nlast: %d\n", myArray.size, myArray.last);
 
   return 0;
 }
